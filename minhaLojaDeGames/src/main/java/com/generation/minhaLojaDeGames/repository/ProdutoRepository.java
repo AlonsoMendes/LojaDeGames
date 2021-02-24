723ddbf595783568245db244a296e556deac5ae2
AMS <alonso_mendess@mail.com>
2021-02-24 16:12:27 -0300

    Atualização do Repository do Produto

--- minhaLojaDeGames/src/main/java/com/generation/minhaLojaDeGames/repository/ProdutoRepository.java
+++ minhaLojaDeGames/src/main/java/com/generation/minhaLojaDeGames/repository/ProdutoRepository.java
@@ -2,10 +2,9 @@ package com.generation.minhaLojaDeGames.repository;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 import com.generation.minhaLojaDeGames.model.Produto;
 
-@Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 	public List<Produto> findAllByNomeProdutoContainingIgnoreCase (String nomeProduto);
 	
