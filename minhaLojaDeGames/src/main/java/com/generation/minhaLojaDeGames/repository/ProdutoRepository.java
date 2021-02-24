ecce381e2bda04e6ddbd1a7d1b192631044f2d59
AMS <alonso_mendess@mail.com>
2021-02-24 15:42:47 -0300

    CRiação do Repository do Produto

--- minhaLojaDeGames/src/main/java/com/generation/minhaLojaDeGames/repository/ProdutoRepository.java
+++ minhaLojaDeGames/src/main/java/com/generation/minhaLojaDeGames/repository/ProdutoRepository.java
@@ -1,5 +1,12 @@
 package com.generation.minhaLojaDeGames.repository;
 
-public interface ProdutoRepository {
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.generation.minhaLojaDeGames.model.Produto;
 
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+	public List<Produto> findAllByNomeProdutoContainingIgnoreCase (String nomeProduto);
+	
 }
